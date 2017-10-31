e9825caf632b6ef51a70be154c235406abcc09c9
mrupavatharam <mrupavatharam@rapidbizapps.com>
2017-10-31 18:37:20 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname,'../public');
+
+
+
+
+console.log(__dirname+'/public', publicPath);
+
+app.use(express.static(publicPath));
+
+app.listen(3000, ()=>{
+    console.log(`Server Started in Port ${port}`);
+})
