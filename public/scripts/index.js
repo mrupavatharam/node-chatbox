8a6f15036c4f5940e6d84d1c9b5a1040e3cf9a0f
mrupavatharam <mrupavatharam@rapidbizapps.com>
2017-11-01 14:03:17 +0530

    Added Scripts Folder in public

---
+++ public/scripts/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+socket.on('connect', function() {
+    console.log("connected to server");
+    socket.emit('createMessage',{
+        to:"suresh",
+        text:"hey hw r u",
+        createdAt: new Date()
+    })
+})
+
+socket.on('disconnect', function(){
+    console.log("Disconnected from server");
+})
+
+socket.on('newMessage', function(message){
+    console.log("Client received new message");
+    console.log(message);
+})
+
